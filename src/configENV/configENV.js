9d9c402d2feea696850c0d07c24e1ab4363842fb
Shreyansh-53 <shreyanshnema1709@gmail.com>
2025-07-08 11:46:39 +0530

    Update configENV.js

--- src/configENV/configENV.js
+++ src/configENV/configENV.js
@@ -4,6 +4,7 @@ const envConfig = {
     appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    tinymceApi: String(import.meta.env.VITE_TINYMCE_API),
 }
 
 export default envConfig
